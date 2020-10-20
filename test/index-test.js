157fcc5be281409634a26603ac73ca8be5ae31f0
Deepthi pasula <deeptineel@gmail.com>
2020-10-20 23:37:19 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -32,14 +32,14 @@ describe('index', () => {
     })
   })
 
-  it('calls console.error("made a mistake!")', done => {
-    const spy = expect.spyOn(console, 'error').andCallThrough()
+  it('calls console.warn("made a mistake!")', done => {
+    const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
       expect(spy).toHaveBeenCalled('expected console.warn to have been called')
-      console.error.restore()
+      console.warn.restore()
       done()
     })
   })
